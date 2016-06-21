a4dcfe9827ff06a3e0a75461a94861b85bda37bc
Chaobin Wu <iamavenwu@gmail.com>
2016-06-21 14:09:57 +0800

    v1.0

---
+++ app/src/main/java/demo/avenwu/net/applymappingdemo/HelloMonica.java
@@ -0,0 +1,14 @@
+package demo.avenwu.net.applymappingdemo;
+
+import android.util.Log;
+
+/**
+ * Created by Chaobin Wu on 6/21/16.
+ */
+
+public class HelloMonica {
+    public void print() {
+        Log.d("Log", "hello");
+    }
+
+}
